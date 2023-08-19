8ba2771bc3fed21ada0de945e25fdb63536de0e8
calebeLAR <ca6ele@gmail.com>
2023-08-19 17:50:19 -0400

    requisito01: refat

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -1,15 +1,28 @@
 """Arquivo que estudantes devem editar"""
 
 
-def hof(context_all_files: [str]) -> int:
+def get_deepest_file(context_all_files: [str]) -> int:
+    # Inicializa um dicionário para armazenar o caminho mais profundo e sua
+    # profundidade
     deeper = {"path": "", "deep": 0}
+
+    # Itera por cada caminho de arquivo na lista de caminhos de arquivos
     for path in context_all_files:
+        # Divide o caminho em partes separadas por "/"
         splited_path = path.split("/")
+
+        # Calcula a profundidade do caminho dividido contando as partes
         deep_path = len(splited_path)
+
+        # Verifica se a profundidade do caminho atual é maior do que a
+        # profundidade armazenada
         if deep_path > deeper["deep"]:
+            # Atualiza o dicionário com a nova profundidade e o novo caminho
+            # mais profundo
             deeper["deep"] = deep_path
             deeper["path"] = path
 
+    # Retorna o caminho mais profundo encontrado
     return deeper["path"]
 
 
@@ -18,7 +31,7 @@ def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = hof(context["all_files"])
+        deepest_file = get_deepest_file(context["all_files"])
         print(f"Deepest file: {deepest_file}")
 
 
