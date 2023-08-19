5e41c9003a7888cd5cf6802558dafbe5c7dc9264
calebeLAR <ca6ele@gmail.com>
2023-08-19 17:42:28 -0400

    requisito01

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -1,11 +1,24 @@
 """Arquivo que estudantes devem editar"""
 
 
+def hof(context_all_files: [str]) -> int:
+    deeper = {"path": "", "deep": 0}
+    for path in context_all_files:
+        splited_path = path.split("/")
+        deep_path = len(splited_path)
+        if deep_path > deeper["deep"]:
+            deeper["deep"] = deep_path
+            deeper["path"] = path
+
+    return deeper["path"]
+
+
+# show_deepest_file req01
 def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = hof(context["all_files"])
         print(f"Deepest file: {deepest_file}")
 
 
