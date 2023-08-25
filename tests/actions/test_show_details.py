e71e9da90558a2518d89866cf525c77f8028705b
calebeLAR <ca6ele@gmail.com>
2023-08-24 23:54:02 -0400

    requisito04: usa fixture tmp_path

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,26 +1,92 @@
 from pro_filer.actions.main_actions import show_details  # NOQA
-import tests.actions.test_show_details_mock as mock
-import pytest
+import os
+from datetime import datetime
 
 
-@pytest.mark.parametrize(
-    "context, saida",
-    [
-        # a chave "base_path" recebe um caminho para um arquivo existente
-        (mock.context01, mock.print01),
-        # a chave "base_path" recebe um caminho para um diretório existente
-        (mock.context02, mock.print02),
-        # a chave "base_path" recebe um caminho para um arquivo sem extenção
-        (mock.context03, mock.print03),
-        # a chave "base_path" recebe um caminho para um arquivo ou diretório
-        # inexistente
-        (mock.context04, mock.print04),
-    ],
-)
-def test_show_details_of_a_file_that_exists(context, saida, capsys, tmp_path):
-    # a função show_details escreve quatro possiveis tipos de saida no
-    # terminal de acordo com o que vem dentro da chave "base_path"
-    # no objeto context
+def test_show_details_of_a_file_that_exists(capsys, tmp_path):
+    # caso caso receba um caminho para um arquivo existente
+    tmp_dir = tmp_path / "dir"
+    tmp_dir.mkdir()
+    tmp_file = tmp_dir / "um_arquivo_existente.txt"
+    tmp_file.write_text("texto do arquivo", encoding="utf-8")
+
+    date = datetime.fromtimestamp(os.path.getmtime(tmp_file))
+    formated_date = date.strftime("%Y-%m-%d")
+
+    context = {}
+    context["base_path"] = str(tmp_file.absolute())
+
     show_details(context)
     readouterr = capsys.readouterr()
-    assert readouterr.out == saida
+
+    assert readouterr.out == (
+        f"""\
+File name: {tmp_file.name}
+File size in bytes: {os.path.getsize(tmp_file)}
+File type: file
+File extension: {tmp_file.suffix}
+Last modified date: {formated_date}\n"""
+    )
+
+
+def test_show_details_of_a_directory_that_exists(capsys, tmp_path):
+    # caso caso receba um caminho para um diretório existente
+    tmp_dir = tmp_path / "dir"
+    tmp_dir.mkdir()
+
+    context = {}
+    context["base_path"] = str(tmp_dir.absolute())
+
+    date = datetime.fromtimestamp(os.path.getmtime(tmp_dir))
+    formated_date = date.strftime("%Y-%m-%d")
+
+    show_details(context)
+    readouterr = capsys.readouterr()
+
+    assert readouterr.out == (
+        f"""\
+File name: {tmp_dir.name}
+File size in bytes: {os.path.getsize(tmp_dir)}
+File type: directory
+File extension: [no extension]
+Last modified date: {formated_date}\n\
+"""
+    )
+
+
+def test_show_details_of_a_file_with_out_extension(capsys, tmp_path):
+    # caso caso receba um caminho para um arquivo sem extenção
+    tmp_file = tmp_path / "arquivo_sem_extenção"
+    tmp_file.touch()
+
+    date = datetime.fromtimestamp(os.path.getmtime(tmp_file))
+    formated_date = date.strftime("%Y-%m-%d")
+
+    context = {}
+    context["base_path"] = str(tmp_file)
+
+    show_details(context)
+    readouterr = capsys.readouterr()
+
+    assert readouterr.out == (
+        f"""\
+File name: {tmp_file.name}
+File size in bytes: {os.path.getsize(tmp_file)}
+File type: file
+File extension: [no extension]
+Last modified date: {formated_date}\n"""
+    )
+
+
+def test_show_details_of_a_file_or_directory_that_not_exists(capsys, tmp_path):
+    # caso caso receba um caminho para um arquivo ou diretório inexistente
+
+    context = {}
+    context["base_path"] = "um_arquivo_ou_diretório_inexistente"
+
+    show_details(context)
+    readouterr = capsys.readouterr()
+
+    assert readouterr.out == (
+        "File 'um_arquivo_ou_diretório_inexistente' does not exist\n"
+    )
