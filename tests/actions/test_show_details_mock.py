4adad621e52c3e60bd74c12c39ad00611d5e4020
calebeLAR <ca6ele@gmail.com>
2023-08-24 23:21:26 -0400

    requisito04: fix - tentativa01

--- tests/actions/test_show_details_mock.py
+++ tests/actions/test_show_details_mock.py
@@ -4,40 +4,40 @@
 context01 = {}
 context01[
     "base_path"
-] = "tests/actions/test_show_details_mock/test_show_details_file.py"
+] = "tests/actions/test_show_details.py"
 # nesse caso ela retona no termial essa string
 print01 = """\
-File name: test_show_details_file.py\n\
-File size in bytes: 1101
+File name: test_show_details.py\n\
+File size in bytes: 1026
 File type: file
 File extension: .py
-Last modified date: 2023-08-24\n\
+Last modified date: 2023-08-25\n\
 """
 
 # caso a chave "base_path" recebe um caminho para um diretório existente
 context02 = {}
-context02["base_path"] = "tests/actions/test_show_details_mock"
+context02["base_path"] = "tests/actions"
 # nesse caso ela retona no termial essa string
 print02 = """\
-File name: test_show_details_mock\n\
+File name: actions\n\
 File size in bytes: 4096
 File type: directory
 File extension: [no extension]
-Last modified date: 2023-08-24\n\
+Last modified date: 2023-08-25\n\
 """
 
 # caso a chave "base_path" recebe um caminho para um arquivo sem extenção
 context03 = {}
 context03[
     "base_path"
-] = "tests/actions/test_show_details_mock/arquivo_sem_extencao"
+] = "tests/actions/test_show_details"
 # nesse caso ela retona no termial essa string
 print03 = """\
-File name: arquivo_sem_extencao\n\
+File name: test_show_details\n\
 File size in bytes: 0
 File type: file
 File extension: [no extension]
-Last modified date: 2023-08-24\n\
+Last modified date: 2023-08-25\n\
 """
 
 # caso a chave "base_path" recebe um caminho para um arquivo ou diretório
